949a95e40444dd9a374ed6a0cb146d5d52268bd3
Yondika Vio Landa <105154819+yondikavl@users.noreply.github.com>
2025-03-08 13:34:07 +0700

    fix: logic if palindrome empty value, ic launcher fix, app name fix

--- lib/app/modules/first_screen/controllers/first_screen_controller.dart
+++ lib/app/modules/first_screen/controllers/first_screen_controller.dart
@@ -12,6 +12,43 @@ class FirstScreenController extends GetxController {
   }
 
   void checkPalindrome() {
+    if (sentence.value.isEmpty) {
+      Get.dialog(
+        AlertDialog(
+          title: const Text('Error'),
+          content: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Input cannot be empty.',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Get.back(),
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                decoration: BoxDecoration(
+                  color: const Color(0xff2B637B),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Text(
+                  'OK',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+
     String resultMessage = isPalindrome(sentence.value)
         ? 'is a palindrome'
         : 'is not a palindrome';
